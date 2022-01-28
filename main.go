987a932a304646133d6132d3376c7059e1183015
aheld <aaron.held@gopuff.com>
2022-01-28 13:55:07 -0500

    step 1

--- main.go
+++ main.go
@@ -3,16 +3,55 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
-func calcScoreForRollInput(rolls string) int {
-	//score := 0
-	//for _, roll := range strings.Split(rolls, ",") {
-	//	rollAsInt, _ := strconv.Atoi(roll)
-	//	score += rollAsInt
-	//}
-	//return score
-	return -1
+type Game struct {
+	frames []Frame
+}
+
+type Frame struct {
+	roll1 int
+	roll2 int
+}
+
+func (f *Frame) Score() int {
+	return f.roll1 + f.roll2
+}
+
+func (f *Frame) IsSpare() bool {
+	return f.roll1+f.roll2 == 10
+}
+
+func (g *Game) Score() int {
+	score := 0
+	for i := 0; i < 10; i++ {
+		score += g.frames[i].Score()
+		if g.frames[i].IsSpare() {
+			score += g.frames[i+1].roll1
+		}
+	}
+	return score
+}
+
+func (g *Game) addFrame(roll1, roll2 int) {
+	g.frames = append(g.frames, Frame{roll1, roll2})
+}
+
+func calcScoreForRollInput(rollInput string) int {
+	game := Game{}
+	rolls := strings.Split(rollInput, ",")
+	for i := 0; i < len(rolls); i += 2 {
+		rollAsInt1, _ := strconv.Atoi(rolls[i])
+		rollAsInt2 := 0
+		if i+1 < len(rolls) {
+			rollAsInt2, _ = strconv.Atoi(rolls[i+1])
+		}
+		game.addFrame(rollAsInt1, rollAsInt2)
+	}
+	fmt.Println(game.frames)
+	return game.Score()
 }
 
 func main() {
--- main_test.go
+++ main_test.go
@@ -13,11 +13,31 @@ func Test(t *testing.T) {
 			input: "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0",
 			score: 0,
 		},
-		//{
-		//	name:  "simple Game",
-		//	input: "1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1",
-		//	score: 20,
-		//},
+		{
+			name:  "simple Game",
+			input: "1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1",
+			score: 20,
+		},
+		{
+			name:  "spare Game",
+			input: "5,5,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0",
+			score: 20,
+		},
+		{
+			name:  "Last roll is a spare",
+			input: "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,9,1,5",
+			score: 15,
+		},
+		// {
+		// 	name:  "Role a strike",
+		// 	input: "10,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0",
+		// 	score: 10,
+		// },
+		// {
+		// 	name:  "Role a strike with a spare at the end",
+		// 	input: "10,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,5,5,5",
+		// 	score: 25,
+		// },
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
